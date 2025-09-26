6606f4abaef8cd8fa862b50bc83d16a4944e55f1
Florian Hirche <flhirche+github@gmail.com>
2025-09-26 22:45:36 +0200

    distance squared method

--- ts/math/vector_util.ts
+++ ts/math/vector_util.ts
@@ -4,4 +4,10 @@ export namespace vector {
   export function distance(vec1: Vector2, vec2: Vector2): number {
     return vec1.subtract(vec2).getMagnitude();
   }
+
+  export function distanceSq(vec1: Vector2, vec2: Vector2): number {
+    let dx = vec1.x - vec2.x;
+    let dy = vec1.y - vec2.y;
+    return dx ** 2 + dy ** 2;
+  }
 }
